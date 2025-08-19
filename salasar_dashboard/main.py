dcd3d67fac53638fbb8e31635fd35c637bece5cc
salasarservices <digitalmarketing@salasarservices.co.in>
2025-08-19 18:10:21 +0530

    Update main.py

--- salasar_dashboard/main.py
+++ salasar_dashboard/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from .components.sidebar import get_sidebar_selection
+from components.sidebar import get_sidebar_selection
 
 # Set page config for wide layout and custom heading
 st.set_page_config(page_title="Salasar Digital Marketing Dashboard", layout="wide")
